a5341af8b9bd42a6c7bda3ab0264c0ca8da3e88e
fkaita <spkaikai@gmail.com>
2022-05-12 12:27:21 +0900

    small_modification

--- scripts/object.py
+++ scripts/object.py
@@ -28,32 +28,31 @@ from trajectory_msgs.msg import JointTrajectoryPoint
 
 
 class ObjectTracker:
-    def __init__(self):      
+    def __init__(self):
         self._bridge = CvBridge()
         self._image_sub = rospy.Subscriber(
             "/sciurus17/camera/color/image_raw", Image, self._image_callback, queue_size=1)
-        self._depth_sub = rospy.Subscriber("/sciurus17/camera/aligned_depth_to_color/image_raw", Image, self._depth_callback, queue_size=1)
+        self._depth_sub = rospy.Subscriber(
+            "/sciurus17/camera/aligned_depth_to_color/image_raw", Image, self._depth_callback, queue_size=1)
 #        self._point_depth = rospy.Subscriber("/sciurus17/camera/depth/image_rect_raw", Image, self._point_depth_callback, queue_size=1)
         self._image_pub = rospy.Publisher("~output_image", Image, queue_size=1)
         self._object_rect = [0, 0, 0, 0]
         self._object_target = [0, 0]
         self._image_shape = Point()
         self._object_detected = False
-        self.dist_target_size = 20 # Estimate distance by mean of size x size pixcels
-        self.round_base = 20 # round by this number in mm
+        self.dist_target_size = 20  # Estimate distance by mean of size x size pixcels
+        self.round_base = 20  # round by this number in mm
         self._face_cascade = cv2.CascadeClassifier(
             "/home/sciurus/Documents/data/haarcascades/haarcascade_frontalface_default.xml")
         self._eyes_cascade = cv2.CascadeClassifier(
             "/home/sciurus/Documents/data/haarcascades/haarcascade_eye.xml")
 
-        
         classFile = '/home/sciurus/Documents/data/coco.names'
         with open(classFile, 'rt') as f:
             self.classNames = [line.rstrip() for line in f]
-        
+
         self._CV_MAJOR_VERSION, _, _ = cv2.__version__.split('.')
 
-        
     def _image_callback(self, ros_image):
         try:
             input_image = self._bridge.imgmsg_to_cv2(ros_image, "bgr8")
@@ -63,42 +62,39 @@ class ObjectTracker:
         # 画像のwidth, heightを取得
         self._image_shape.x = input_image.shape[1]
         self._image_shape.y = input_image.shape[0]
-        
+
         self.input_img = input_image
 
         self._image_pub.publish(self._bridge.cv2_to_imgmsg(input_image, "bgr8"))
-        
+
     def _depth_callback(self, ros_image):
         try:
             input_image = self._bridge.imgmsg_to_cv2(ros_image, "passthrough")
         except CvBridgeError as e:
             rospy.logerr(e)
             return
-            
+
         # 画像のwidth, heightを取得
         self._image_shape.x = input_image.shape[1]
         self._image_shape.y = input_image.shape[0]
-        
-        
+
         self.depth_img = np.array(input_image)
-       
+
         # output_image = self._detect_object(input_image)
         # if output_image is not False:
         #     self._image_pub.publish(self._bridge.cv2_to_imgmsg(output_image, "bgr8"))
 
         # self._median_depth_pub.publish(self._median_depth)
-        
+
 #    def _point_depth_callback(self, ros_image):
 #        try:
 #            input_image = self._bridge.imgmsg_to_cv2(ros_image, "passthrough")
 #        except CvBridgeError as e:
 #            rospy.logerr(e)
-#            return    
-#        
+#            return
+#
 #        self.point_depth_img = np.array(input_image)
 #        print(self.point_depth_img[100,100])
-
-
 
     def get_object_position(self):
         # 画像中心を0, 0とした座標系におけるオブジェクトの座標を出力
@@ -122,43 +118,44 @@ class ObjectTracker:
             normalized_point.y = translated_point.y / (self._image_shape.y * 0.5)
 
         return normalized_point
-        
 
     def object_detected(self):
         return self._object_detected
 
-
     def _detect_object(self, net):
         img = self.input_img
-        
+
         classIds, confs, bbox = net.detect(img, confThreshold=0.5)
 
         if len(classIds) != 0:
             for classId, confidence, box in zip(classIds.flatten(), confs.flatten(), bbox):
-                
+
                 center = [int((box[0]+box[2])/2), int((box[1]+box[3])/2)]
                 size = int(self.dist_target_size/2)
-                target_depth = self.depth_img[center[0]-size:center[0]+size, center[1]-size:center[1]+size]
+                target_depth = self.depth_img[center[0] -
+                                              size:center[0]+size, center[1]-size:center[1]+size]
 #                target_depth = self.round_base * np.round(target_depth/self.round_base)
 #                val, count = np.unique(target_depth, return_counts=True)
 #                distance_mm = val[np.argmax(count)]
-                distance_mm = np.round(np.mean(target_depth[target_depth>1]),-1)
-                
-                cv2.putText(img, "{} mm".format(distance_mm), (box[0], box[1]+15), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 255, 0), 1)
+                distance_mm = np.round(np.mean(target_depth[target_depth > 1]), -1)
+
+                cv2.putText(img, "{} mm".format(distance_mm),
+                            (box[0], box[1]+15), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 255, 0), 1)
                 cv2.rectangle(img, box, color=(0, 255, 0), thickness=2)
-                cv2.putText(img, self.classNames[classId-1].upper(), (box[0], box[1]), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 255, 0), 1)
+                cv2.putText(img, self.classNames[classId-1].upper(), (box[0],
+                            box[1]), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 255, 0), 1)
                 # cv2.putText(img, str(round(confidence*100, 2)), (box[0]+150, box[1]), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 255, 0), 1)
             cv2.imshow('output', img)
             cv2.waitKey(1)
             self._object_detected = True
-            self._object_target = (1,1)
-        
+            self._object_target = (1, 1)
+
         return img
-        
+
     def _detect_color_object(self, bgr_image, lower_color, upper_color):
         # 画像から指定された色の物体を検出する
 
-        MIN_OBJECT_SIZE = 1000 # px * px
+        MIN_OBJECT_SIZE = 1000  # px * px
 
         # BGR画像をHSV色空間に変換
         hsv = cv2.cvtColor(bgr_image, cv2.COLOR_BGR2HSV)
@@ -187,11 +184,11 @@ class ObjectTracker:
             # 長方形が小さければ検出判定にしない
             if rect[2] * rect[3] > MIN_OBJECT_SIZE:
                 # 抽出した長方形を画像に描画する
-                cv2.rectangle(bgr_image, 
-                        (rect[0], rect[1]), 
-                        (rect[0] + rect[2], rect[1] + rect[3]), 
-                        (0, 0, 255), thickness=2)
-                        
+                cv2.rectangle(bgr_image,
+                              (rect[0], rect[1]),
+                              (rect[0] + rect[2], rect[1] + rect[3]),
+                              (0, 0, 255), thickness=2)
+
                 cv2.imshow('output', bgr_image)
                 cv2.waitKey(1)
 
@@ -200,19 +197,18 @@ class ObjectTracker:
 
         return bgr_image
 
-
     def _detect_lblue_object(self):
         img = self.input_img
         # H: 0 ~ 179 (0 ~ 360°)
         # S: 0 ~ 255 (0 ~ 100%)
         # V: 0 ~ 255 (0 ~ 100%)
-        lower_lblue = np.array([0,130,200])
-        upper_lblue = np.array([150,220,255])
+        lower_lblue = np.array([0, 130, 200])
+        upper_lblue = np.array([150, 220, 255])
 
-        return self._detect_color_object(img, lower_lblue, upper_lblue
+        return self._detect_color_object(img, lower_lblue, upper_lblue)
 
     def _detect_head_direct(self):
-    
+
         img = self.input_img
 
         detector = dlib.get_frontal_face_detector()
@@ -243,7 +239,7 @@ class ObjectTracker:
         focal_length = size[1]
         center = (size[1]/2, size[0]/2)
         camera_matrix = np.array([[focal_length, 0, center[0]], [
-                                 0, focal_length, center[1]], [0, 0, 1]], dtype="double")
+            0, focal_length, center[1]], [0, 0, 1]], dtype="double")
 
         self._object_detected = False
 
@@ -285,7 +281,6 @@ class ObjectTracker:
         return bgr_image
 
 
-
 def main():
     r = rospy.Rate(10)
     configPath = '/home/sciurus/Documents/data/ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt'
@@ -295,17 +290,15 @@ def main():
     net.setInputScale(1.0 / 127.5)
     net.setInputMean((127.5, 127.5, 127.5))
     net.setInputSwapRB(True)
-    
-    
+
     while not rospy.is_shutdown():
-#        object_position = object_tracker._detect_object(net)
-#        object_position = object_tracker._detect_lblue_object()
+        #        object_position = object_tracker._detect_object(net)
+        #        object_position = object_tracker._detect_lblue_object()
         object_position = object_tracker._detect_head_direct()
-    
-    
+
+
 if __name__ == '__main__':
     rospy.init_node("head_camera_tracking")
     object_tracker = ObjectTracker()
-    
-    main()
 
+    main()
